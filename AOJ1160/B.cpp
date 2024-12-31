898d84f1fb70930280e2581c82f3b05bd42baec6
SK <sonodans109@gmail.com>
2024-12-31 19:54:11 +0900

    modified AOJ1160 B

--- AOJ1160/B.cpp
+++ AOJ1160/B.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -8,29 +9,39 @@
 #include <set>
 
 using namespace std;
+//using LL = long long;
 
-int Map[51][51];
-bool visited[51][51];
+const int EREA_RANGE = 50; //h, wの最大値
+int world_Map[EREA_RANGE + 1][EREA_RANGE + 1]; //0:海 1:地
+int num_Map[EREA_RANGE + 1][EREA_RANGE + 1]; //エリア[h][w]を数値で定義する。
 
-const int DIFF[8][2] = { {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1 ,0}, {-1 ,1} };
-int W, H;
+/*
+[1][1]	[1][2]	[1][3]	[1][4]
+[2][1]	[2][2]	[2][3]	[2][4]
+[3][1]	[3][2]	[3][3]	[3][4]
+
+1	2	3	4
+5	6	7	8
+9	10	11	12
+*/
+
+bool visited[EREA_RANGE * EREA_RANGE + 1]; //訪問済みのエリア
+const int MOVE_NUM = 8;
+const int DIFF[MOVE_NUM][2] = { {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1} }; //現在のエリアから移動可能のエリアの方向
+
+int H, W;
+int ans;
+vector<vector<int>> Graph(EREA_RANGE * EREA_RANGE + 1); //接続されている各頂点
 
 
-void dfs(int h, int w) {
+void dfs(int num) {
 
-	visited[h][w] = true;
+	visited[num] = true;
 
-	for (int i = 0; i < 8; i++) {
+	for (int itr = 0; itr < Graph[num].size(); itr++) {
 
-		int search_h = h + DIFF[i][0];
-		int search_w = w + DIFF[i][1];
-
-		if (search_h < 1 || search_h > H)continue;
-		if (search_w < 1 || search_w > W)continue;
-
-		if (visited[search_h][search_w] == false && Map[search_h][search_w] == 1) {
-			dfs(search_h, search_w);
-		}
+		if (visited[Graph[num][itr]] == false)
+			dfs(Graph[num][itr]);
 	}
 
 	return;
@@ -38,11 +49,16 @@ void dfs(int h, int w) {
 
 void clear() {
 
-	for (int h = 1; h <= 50; h++) {
-		for (int w = 1; w <= 50; w++) {
-			Map[h][w] = 0;
-			visited[h][w] = false;
+	for (int h = 1; h <= EREA_RANGE; h++) {
+		for (int w = 1; w <= EREA_RANGE; w++) {
+			world_Map[h][w] = 0;
+			num_Map[h][w] = 0;
 		}
+	}
+
+	for (int i = 1; i <= EREA_RANGE * EREA_RANGE; i++) {
+		visited[i] = false;
+		Graph[i].clear();
 	}
 }
 
@@ -50,7 +66,6 @@ int main()
 {
 
 	while (true) {
-
 		cin >> W >> H;
 
 		if (W == 0 && H == 0)
@@ -58,28 +73,47 @@ int main()
 
 		clear();
 
+		int num = 1;
 		for (int h = 1; h <= H; h++) {
 			for (int w = 1; w <= W; w++) {
-				cin >> Map[h][w];
-				visited[h][w] = false;
+				cin >> world_Map[h][w];
+				num_Map[h][w] = num;
+				num++;
 			}
 		}
 
-		int land_num = 0;
-
 		for (int h = 1; h <= H; h++) {
 			for (int w = 1; w <= W; w++) {
 
-				if (visited[h][w] == false && Map[h][w] == 1) {
-					land_num++;
-					dfs(h, w);
+				if (world_Map[h][w] == 1) {
+					for (int i = 0; i < 8; i++) {
+						int search_h = h + DIFF[i][0];
+						int search_w = w + DIFF[i][1];
+
+						if (search_h < 1 || search_h > H)continue;
+						if (search_w < 1 || search_w > W)continue;
+
+						if (world_Map[search_h][search_w] == 1)
+							Graph[num_Map[h][w]].push_back(num_Map[search_h][search_w]);
+					}
 				}
 			}
 		}
 
-		cout << land_num << endl;
-	}
+		ans = 0;
 
+		for (int h = 1; h <= H; h++) {
+			for (int w = 1; w <= W; w++) {
+				
+				if (visited[num_Map[h][w]] == false && world_Map[h][w] == 1) {
+					ans++;
+					dfs(num_Map[h][w]);
+				}
+			}
+		}
+
+		cout << ans << endl;
+	}
 
 	return 0;
 }
